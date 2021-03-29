a935e19bfa5a25023c06169dbf199874936de864
Colleen Handel <handelc22@gmail.com>
2021-03-29 17:38:27 -0500

    Implemented webpack and babel

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('test');
-})
+app.use(express.static('client'));
+
+// app.get('/', (req, res) => {
+//   res.send('test');
+// })
 
 app.listen(2000, () => {
   console.log('server listening on port 2000');
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/app.js',
+  output: {
+    path: path.resolve(__dirname, 'client'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+}
